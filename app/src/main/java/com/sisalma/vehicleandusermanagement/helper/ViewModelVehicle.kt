a2131a461f6c8f928d8b8658f6ab043c1357ba15
Ariq <bitcoin1192@gmail.com>
2022-05-22 22:25:29 +0700

    First step to store member data for selected VID: Add Livedata for later use

--- app/src/main/java/com/sisalma/vehicleandusermanagement/helper/ViewModelVehicle.kt
+++ app/src/main/java/com/sisalma/vehicleandusermanagement/helper/ViewModelVehicle.kt
@@ -7,8 +7,9 @@ import androidx.lifecycle.ViewModel
 class ViewModelVehicle: ViewModel() {
     private var selectedVID = ""
     private val _requestMemberData: MutableLiveData<String> = MutableLiveData()
+    private val _vehicleMemberData: MutableLiveData<String> = MutableLiveData()
     val requestMemberData: LiveData<String> get() = _requestMemberData
-
+    val vehicleMemberData: LiveData<String> get() = _vehicleMemberData
     fun setVID(input: String){
         selectedVID = input
         _requestMemberData.value = selectedVID
